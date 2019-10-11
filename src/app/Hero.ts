de73a0d3e53d66a0318d143b2d556119b858a55d
Jawen Voon <weinchester@gmail.com>
2019-10-11 11:13:31 +0800

    reformat some stuff

--- src/app/Hero.ts
+++ src/app/Hero.ts
@@ -2,3 +2,16 @@ export class Hero{
     id: number;
     name: string;
 }
+
+export const HEROES: Hero[] =[
+    {id: 11, name:'Dr Nice'},
+    {id: 12, name:'Batman'},
+    {id: 13, name:'Bonbasto'},
+    {id: 14, name:'Celeritas'},
+    {id: 15, name:'Magneto'},
+    {id: 16, name:'Elastic Man'},
+    {id: 17, name:'Dynamo'},
+    {id: 18, name:'Shark Boy'},
+    {id: 19, name:'Ant Man'},
+    {id: 20, name:'Fury'},
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeroesComponent } from './heroes/heroes.component';
+import { fromEventPattern } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
