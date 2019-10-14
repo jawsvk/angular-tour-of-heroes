55ffff63396f56bfd496e4fabbb7783ef8c199b3
Jawen Voon <weinchester@gmail.com>
2019-10-14 16:55:23 +0800

    put log in tap of pipe

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,16 +18,21 @@ export class HeroService {
     private messageService: MessageService) { }
 
   getHeroes = (): Observable<Hero[]> => {
-    this.log('fetched heroes');
     return this.http.get<Hero[]>(this.heroesUrl)
     .pipe(
+      tap(_ => this.log('fetched heroes')),
       catchError(this.handleError<Hero[]>('getHeroes', []))
     );
   }
 
   getHero = (id:number): Observable<Hero> => {
-    this.log(`fetched hero id = ${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.get<Hero>(url)
+    .pipe(
+      tap(_ => this.log(`getHero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
   }
 
   private log(message: string) {
@@ -36,14 +41,9 @@ export class HeroService {
 
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-  
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-  
-      // TODO: better job of transforming error for user consumption
+
+      console.error(error);
       this.log(`${operation} failed: ${error.message}`);
-  
-      // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
