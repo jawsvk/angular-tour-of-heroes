d373d1b275009cf30892c568a0bbc989738c1dc8
Jawen Voon <weinchester@gmail.com>
2019-10-29 17:23:53 +0800

    add tests for message service

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -3,10 +3,29 @@ import { TestBed } from '@angular/core/testing';
 import { MessageService } from './message.service';
 
 describe('MessageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
+  let service: MessageService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(MessageService);
+  });
+  
   it('should be created', () => {
-    const service: MessageService = TestBed.get(MessageService);
     expect(service).toBeTruthy();
   });
+
+  it('should add message to messages', () =>{
+    expect(service.messages.length).toEqual(0);
+    service.add('test message');
+    expect(service.messages[0]).toEqual('test message');
+  });
+
+  it('should clear mesages', () =>{
+    expect(service.messages.length).toEqual(0);
+    service.add('test message');
+    service.clear();
+    expect(service.messages.length).toEqual(0);
+
+  });
+
+
 });
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -11,6 +11,7 @@ export class MessageService {
 
   add = (message: String):void => {
     this.messages.push(message)
+    
   }
 
   clear = () => {
