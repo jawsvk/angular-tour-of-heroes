94079607b4173f8fa15235a98b47a4cec0f09321
Jawen Voon <weinchester@gmail.com>
2019-10-30 11:47:09 +0800

    inject mock message service

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,14 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 describe('HeroService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule],
-  }));
+  let service: HeroService
+  let messageServiceSpy: jasmine.SpyObj<MessageService>;
+
+  beforeEach(() => {
+    const spy =  jasmine.createSpyObj('MessageService', ['add']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HeroService, {provide: MessageService, useValue: spy}],
+    });
+    service = TestBed.get(HeroService);
+    messageServiceSpy = TestBed.get(MessageService);
+
+});
 
   it('should be created', () => {
-    const service: HeroService = TestBed.get(HeroService);
     expect(service).toBeTruthy();
   });
+
+  it('should create add spy method', ()=>{
+    expect(messageServiceSpy.add).toBeDefined();
+  });
+
 });
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,6 +18,19 @@ export class HeroService {
     })
   };
 
+  heroes = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService
