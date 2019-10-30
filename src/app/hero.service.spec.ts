13a4ddc7dfa455aa7f2425c017c04176611e3810
Jawen Voon <weinchester@gmail.com>
2019-10-30 14:23:23 +0800

    add http get request test

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -3,20 +3,37 @@ import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { async } from 'q';
+import { of } from 'rxjs';
+
+const expectedHeroes =  [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 describe('HeroService', () => {
   let service: HeroService
+  let httpClientSpy: { get: jasmine.Spy }
   let messageServiceSpy: jasmine.SpyObj<MessageService>;
 
   beforeEach(() => {
     const spy =  jasmine.createSpyObj('MessageService', ['add']);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
 
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [HeroService, {provide: MessageService, useValue: spy}],
     });
-    service = TestBed.get(HeroService);
     messageServiceSpy = TestBed.get(MessageService);
+    service = new HeroService(<any> httpClientSpy, messageServiceSpy);
 
 });
 
@@ -24,8 +41,18 @@ describe('HeroService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should create add spy method', ()=>{
+  it('should create add spy method', () => {
     expect(messageServiceSpy.add).toBeDefined();
+  });
+
+  it('should return array of all heroes (HttpClient called once)', () => {
+
+    httpClientSpy.get.and.returnValue(of(expectedHeroes));
+    service.getHeroes().subscribe(
+      heroes => expect(heroes).toEqual(expectedHeroes, 'expected heroes'), fail
+    );
+
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
   });
 
 });
